852df842d13483b85882c42baed498365bfcaf72
AJ <acjones@protonmail.com>
2019-12-05 20:33:53 +0000

    maybe like this

--- src/day5/intcode_executor.py
+++ src/day5/intcode_executor.py
@@ -52,7 +52,7 @@ class IntCode:
             Op.JUMP_IF_FALSE: lambda: (
                 val := self.get_parameter_value(),
                 where := self.get_parameter_value(),
-                self.instruction_pointer := where if val == 0)
+                self.instruction_pointer := where if val == 0),
             Op.EQUALS: lambda: self.set_at_parameter(int(self.get_parameter_value() == self.get_parameter_value())),
             Op.LESS_THAN: lambda: self.set_at_parameter(int(self.get_parameter_value() < self.get_parameter_value()))
             Op.STOP: lambda: False
